3f86134db921f3c5e2cc4d4948aa3ddc2a5f6f1d
gio-gervaz <geovaspablo9@gmail.com>
2026-08-06 22:05:32 -0600

    modo oscuro

--- script.js
+++ script.js
@@ -11,3 +11,52 @@ function mostrarPromo(enlace){
     enlace.textContent = "Ver más"; 
     enlace.classList.toggle("abierto");
 }
+
+
+
+
+const switchDark = document.querySelector("#darkMode");
+const logo = document.getElementById("logo");
+
+// recuperar modo guardado
+if (localStorage.getItem("modo") === "oscuro") {
+
+    document.body.classList.add("dark-mode");
+    
+    if (switchDark) {
+        switchDark.checked = true;
+    }
+    if (logo) {
+        logo.src = "img/logo negro.png";
+    }
+
+} else {
+    if (logo) {
+        logo.src = "img/logo blanco.png";
+    }
+}
+
+
+// solo activar cambio si existe el switch
+if (switchDark) {
+
+    switchDark.addEventListener("change", () => {
+
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+
+            if (logo) {
+                logo.src = "img/logo negro.png";
+            }
+
+            localStorage.setItem("modo", "oscuro");
+
+        } else {
+            if (logo) {
+                logo.src = "img/logo blanco.png";
+            }
+            localStorage.setItem("modo", "claro");
+        }
+    });
+}
